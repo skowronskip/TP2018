df80e2251de5eecb333707fdb9af5c045f74c479
Paweł Skowroński <robert225.rs@gmail.com>
2018-10-11 08:56:02 +0200

    initial commit

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Book
+    {
+        private String title;
+        private String author;
+
+        public Book (String title, String author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public String getTitle()
+        {
+            return this.title;
+        }
+
+        public String getAuthor()
+        {
+            return this.author;
+        }
+    }
+}
